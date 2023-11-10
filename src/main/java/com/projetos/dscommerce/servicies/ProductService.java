19fc5ec9f2e19f88ce011366c77be9edc68f798b
Andre M. de Souza <andresouza44@gmail.com>
2023-11-10 10:58:41 -0300

    GET prodructs paged

--- src/main/java/com/projetos/dscommerce/servicies/ProductService.java
+++ src/main/java/com/projetos/dscommerce/servicies/ProductService.java
@@ -4,10 +4,13 @@ import com.projetos.dscommerce.dto.ProductDTO;
 import com.projetos.dscommerce.entities.Product;
 import com.projetos.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.swing.text.html.Option;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -21,6 +24,12 @@ public class ProductService {
         Product product =  repository.findById(id).get();
         return new ProductDTO(product);
 
+    }
 
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable){
+        Page<Product> result= repository.findAll(pageable);
+        return result.map(e -> new ProductDTO(e)); // Page já um stream
     }
 }
+
