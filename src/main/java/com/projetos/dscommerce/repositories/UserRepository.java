0007f3968e55577fad7a7839ed7dc51aa4a8d4f8
Andre M. de Souza <andresouza44@gmail.com>
2023-12-05 18:41:51 -0300

    UserDetailsService

--- src/main/java/com/projetos/dscommerce/repositories/UserRepository.java
+++ src/main/java/com/projetos/dscommerce/repositories/UserRepository.java
@@ -20,7 +20,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 				INNER JOIN tb_role ON tb_role.id = tb_user_role.role_id
 				WHERE tb_user.email = :email
 			""")
-    List<UserDetailsProjection> searchUserAndRolesByEmail(String userEmail);
+    List<UserDetailsProjection> searchUserAndRolesByEmail(String email);
 
 
 }
---
+++ src/main/java/com/projetos/dscommerce/servicies/UserService.java
@@ -0,0 +1,39 @@
+package com.projetos.dscommerce.servicies;
+
+import com.projetos.dscommerce.entities.Role;
+import com.projetos.dscommerce.entities.User;
+import com.projetos.dscommerce.projections.UserDetailsProjection;
+import com.projetos.dscommerce.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        List<UserDetailsProjection> result = repository.searchUserAndRolesByEmail(username);
+        if (result.size() == 0 ){
+            throw  new UsernameNotFoundException("User no Found");
+        }
+
+
+        User user = new User();
+        user.setEmail(result.get(0).getUsername());
+        user.setPassword(result.get(0).getPassword());
+        result.forEach(e -> user.addRole(new Role(e.getRoleId(), e.getAuthority())));
+
+        return user;
+
+
+    }
+}
